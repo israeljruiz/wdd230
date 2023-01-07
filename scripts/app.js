ea10624bc95b9dfd7e008bbb0be79dfcb90de8a4
israeljruiz <israeljruiz@gmail.com>
2023-01-06 23:42:05 -0500

    Doing good!

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
 const copyright = document.querySelector('#copyright');
 const currentYear = new Date().getFullYear();
 copyright.textContent = `©️ ${new Date().getFullYear()}`;
+
+//`Current Date: ${dayName}, ${monthName} ${d.getDate()}, ${year}`;
+
+let quantity = document.querySelector('#q').value;
