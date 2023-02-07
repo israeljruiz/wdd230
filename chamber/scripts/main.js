59eec62698f258ecf6818d657cdcdd936f8e2583
israeljruiz <israeljruiz@gmail.com>
2023-02-07 12:37:03 -0500

    on my way

---
+++ chamber/scripts/main.js
@@ -0,0 +1,12 @@
+const datefield = document.querySelector('#date');
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format();
+datefield.textContent = fullDate;
+
+const catchLastModification = new Date(document.lastModified);
+const yearMonthDay = catchLastModification.toLocaleString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'});
+const hourMinutesSeconds = catchLastModification.toLocaleString('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+const dateTime = `Last updated: ${yearMonthDay} ${hourMinutesSeconds} `;
+document.getElementById('updated').innerHTML = dateTime
+
+document.getElementById('year').innerHTML = `&copy; ${new Date().getFullYear()}`;
